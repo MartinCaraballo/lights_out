40abbd5e51ba88341d7a9d4ca4146229ebb8f689
tomaspasto24 <tomaspasto24@gmail.com>
2023-11-08 14:07:39 -0300

    .

--- Screens/screen.py
+++ Screens/screen.py
@@ -1,6 +1,43 @@
 import tkinter as tk
 from tkinter import messagebox
 
+def show(matriz):
+    def toggle_light(row, col):
+        print('sd')
+    def update_lights():
+        for row in range(filas):
+            for col in range(columnas):
+                if matriz[row][col] == 1:
+                    canvas.itemconfig(shapes[row][col], fill='yellow')
+                else:
+                    canvas.itemconfig(shapes[row][col], fill='white')
+    ventana = tk.Tk()
+    ventana.title("Matriz resultado")
+
+    # Definir el tamaño de la cuadrícula.
+    filas = len(matriz)
+    columnas = len(matriz[0])
+
+    # Crear un lienzo (canvas) para mostrar las formas.
+    canvas = tk.Canvas(ventana, width=200, height=200)
+    canvas.pack()
+
+    # Crear formas geométricas para representar las luces.
+    shapes = [[None for _ in range(columnas)] for _ in range(filas)]
+
+    for row in range(filas):
+        for col in range(columnas):
+            x1, y1 = col * 50, row * 50
+            x2, y2 = x1 + 50, y1 + 50
+            shapes[row][col] = canvas.create_rectangle(x1, y1, x2, y2, fill='white')
+            canvas.tag_bind(shapes[row][col], '<Button-1>', lambda event, r=row, c=col: toggle_light(r, c))
+
+    # Inicializar la apariencia de las luces según la matriz dada.
+    update_lights()
+
+    # Ejecutar la aplicación.
+    ventana.mainloop()
+    
 def open_screen(num):
     # Función para cambiar el estado de una luz y actualizar su apariencia.
     def toggle_light(row, col):
@@ -17,7 +54,10 @@ def open_screen(num):
     # Crear la ventana de la aplicación.
     ventana = tk.Tk()
     ventana.title("Lights Out")
-
+    def mostrar_solucion():
+        show([[0, 0, 0, 1], [0, 1, 0, 1], [0, 0, 0, 0], [0, 1, 0, 0]]);
+    resultado_boton = tk.Button(ventana, text="Mostrar Solución", command=mostrar_solucion)
+    resultado_boton.pack()
     # Definir el tamaño de la cuadrícula y la cuadrícula de luces (ajusta según tus preferencias).
     filas = int(num)
     columnas = int(num)
@@ -39,8 +79,6 @@ def open_screen(num):
             y2 = y1 + 80
             shapes[row][col] = canvas.create_rectangle(x1, y1, x2, y2, fill='white')
             canvas.tag_bind(shapes[row][col], '<Button-1>', lambda event, r=row, c=col: toggle_light(r, c))
-
-    # Puedes inicializar las luces de manera aleatoria o como desees.
 
     # Ejecutar la aplicación.
     ventana.mainloop()
@@ -74,4 +112,9 @@ def init():
 
     ventanaPrevia.mainloop()
 
-init();
+init()
+
+
+
+
+# 
